0e6e97e82c878061e58e0db34e28633764144cfe
Jacob Mossberg <jacob@jacobmossberg.se>
2022-10-06 11:37:43 +0200

    Add SighticAnalytics Swift Package

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SighticAnalytics",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "SighticAnalytics",
+            targets: ["SighticAnalytics"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "SighticAnalytics",
+            path: "https://github.com/EyescannerTechnology/sightic-sdk-ios/releases/download/0.0.1/SighticAnalytics.xcframework.zip",
+            checksum: "04def5b9ee73120c615d74dc41622b4cff116273a20ec3f391ea0e4ba1cae09d")
+    ]
+)
