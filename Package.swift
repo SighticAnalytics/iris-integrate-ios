da7debb8d97cc6175b381286ddefd2295a79ed67
Jacob Mossberg <jacob@jacobmossberg.se>
2022-10-06 12:38:43 +0200

    Change from key path to url in binaryTarget in Package.swift.

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SighticAnalytics",
-            path: "https://github.com/EyescannerTechnology/sightic-sdk-ios/releases/download/0.0.1/SighticAnalytics.xcframework.zip",
+            url: "https://github.com/EyescannerTechnology/sightic-sdk-ios/releases/download/0.0.1/SighticAnalytics.xcframework.zip",
             checksum: "04def5b9ee73120c615d74dc41622b4cff116273a20ec3f391ea0e4ba1cae09d")
     ]
 )
