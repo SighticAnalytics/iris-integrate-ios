4dfb1d0402acaaf96e7b72d7fdc0de839b44ed9d
Marcus Hedenström <marcus.hedenstrom@eyescanner.com>
2022-10-06 15:06:30 +0200

    Version 0.0.5 for iPhone hardware

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SighticAnalytics",
-            url: "https://github.com/EyescannerTechnology/sightic-sdk-ios/releases/download/0.0.4/SighticAnalytics.xcframework.zip",
-            checksum: "0b2203a19aba6320277d109e0e08b6b0adcb8081c844de7f2b5b2804ccbe0d0d")
+            url: "https://github.com/EyescannerTechnology/sightic-sdk-ios/releases/download/0.0.5/SighticAnalytics.xcframework.zip",
+            checksum: "5ac6ed49765190235f82ea09e6976a651b1cf69178f297ce1306f35f814ea748")
     ]
 )
