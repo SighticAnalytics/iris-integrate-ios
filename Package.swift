fc52d6a64289ebddfb2f1a09719576c090f14a3a
GitHub <datainsamling@eyescanner.se>
2022-11-11 13:38:53 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,6 +17,6 @@ let package = Package(
         .binaryTarget(
             name: "SighticAnalytics",
             url: "https://github.com/EyescannerTechnology/sightic-sdk-ios/releases/download/1123/SighticAnalytics.xcframework.zip",
-            checksum: "ef819b4f54d309e3dd684d23bed55dd610548d15870936fe856a02fa9dbcec36")
+            checksum: "c20a22e3a811877960448f42887ca5f2530da2b0d4b5b6bfbb825e64cf1e7d1e")
     ]
 )     
